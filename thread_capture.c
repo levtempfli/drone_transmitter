f5d409685a34300e82921bfb2418067d9c261715
Tempfli Levente <2000levi2000@gmail.com>
2020-09-03 19:45:58 +0300

    Thread_capture module removed
    I don't know who left this here

--- thread_capture.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-
-void *thr_capture_main(void *ptr) {
-    printf("I am capture thread\n");
-    return NULL;
-}
--- thread_capture.h
+++
@@ -1,6 +0,0 @@
-#ifndef THREAD_CAPTURE_H
-#define THREAD_CAPTURE_H
-
-extern void *thr_capture_main(void *ptr);
-
-#endif //THREAD_CAPTURE_H
