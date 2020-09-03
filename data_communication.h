f3dc447106632acc0e8b97e6c04e80606acbd53c
Tempfli Levente <2000levi2000@gmail.com>
2020-09-03 13:00:03 +0300

    Some data struct corrections
    The data struct had to be corrected:
    1. Rename from data_telemetry to data_communication
    2. Some initialization was missing
    3. Struct definition moved from .c to .h
    4. msg_len is now included in the struct

--- data_communication.h
+++ data_communication.h
@@ -7,6 +7,15 @@
 #define TELEM_MSG_NUM 7
 #define CONTR_MSG_NUM 3
 
+struct data_comm_strc {
+    struct msg_mtx_ready_strc {
+        char msg[MAX_MSG_SIZE];
+        int msg_len;
+        pthread_mutex_t mtx;
+        char ready;
+    } telem_msgs[TELEM_MSG_NUM], control_msgs[CONTR_MSG_NUM];
+};
+
 extern struct data_comm_strc data_comm;
 
 extern int init_data_comm_struct();
